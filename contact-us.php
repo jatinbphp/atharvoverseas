5b1a58496913085a6ee6da95c592274b59c881a3
Jatin Bhatt <jatin.b.php@gmail.com>
2023-07-14 19:02:03 +0530

    changes

--- contact-us.php
+++ contact-us.php
@@ -1,5 +1,4 @@
 <?php include 'includes/header.php' ?>
-<?php include 'includes/home-slider.php' ?>
 <div id="Content">
    <div class="content_wrapper clearfix">
       <div class="sections_group">
