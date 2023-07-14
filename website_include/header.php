62d2ca90a5645a04180d096b836f545414a12eaf
Jatin Bhatt <jatin.b.php@gmail.com>
2023-07-14 18:21:26 +0530

    add css

--- website_include/header.php
+++ website_include/header.php
@@ -85,33 +85,9 @@
         <link rel="stylesheet" type="text/css" href="https://atharvoverseas.com/wp-content/plugins/js_composer/assets/css/vc-ie8.min.css" media="screen">
         <![endif]-->
         <meta name="generator" content="Powered by Slider Revolution 6.1.0 - responsive, Mobile-Friendly Slider Plugin for WordPress with comfortable drag and drop interface." />
-        <script type="text/javascript">function setREVStartSize(a){try{var b,c=document.getElementById(a.c).parentNode.offsetWidth;if(c=0===c||isNaN(c)?window.innerWidth:c,a.tabw=void 0===a.tabw?0:parseInt(a.tabw),a.thumbw=void 0===a.thumbw?0:parseInt(a.thumbw),a.tabh=void 0===a.tabh?0:parseInt(a.tabh),a.thumbh=void 0===a.thumbh?0:parseInt(a.thumbh),a.tabhide=void 0===a.tabhide?0:parseInt(a.tabhide),a.thumbhide=void 0===a.thumbhide?0:parseInt(a.thumbhide),a.mh=void 0===a.mh||""==a.mh?0:a.mh,"fullscreen"===a.layout||"fullscreen"===a.l)b=Math.max(a.mh,window.innerHeight);else{for(var d in a.gw=Array.isArray(a.gw)?a.gw:[a.gw],a.rl)(void 0===a.gw[d]||0===a.gw[d])&&(a.gw[d]=a.gw[d-1]);for(var d in a.gh=void 0===a.el||""===a.el||Array.isArray(a.el)&&0==a.el.length?a.gh:a.el,a.gh=Array.isArray(a.gh)?a.gh:[a.gh],a.rl)(void 0===a.gh[d]||0===a.gh[d])&&(a.gh[d]=a.gh[d-1]);var e,f=Array(a.rl.length),g=0;for(var d in a.tabw=a.tabhide>=c?0:a.tabw,a.thumbw=a.thumbhide>=c?0:a.thumbw,a.tabh=a.tabhide>=c?0:a.tabh,a.thumbh=a.thumbhide>=c?0:a.thumbh,a.rl)f[d]=a.rl[d]<window.innerWidth?0:a.rl[d];for(var d in e=f[0],f)e>f[d]&&0<f[d]&&(e=f[d],g=d);var h=c>a.gw[g]+a.tabw+a.thumbw?1:(c-(a.tabw+a.thumbw))/a.gw[g];b=a.gh[g]*h+(a.tabh+a.thumbh)}void 0===window.rs_init_css&&(window.rs_init_css=document.head.appendChild(document.createElement("style"))),document.getElementById(a.c).height=b,window.rs_init_css.innerHTML+="#"+a.c+"_wrapper { height: "+b+"px }"}catch(a){console.log("Failure at Presize of Slider:"+a)}};</script>
-        <style type="text/css" id="wp-custom-css">
-            .header-classic #Action_bar,.header-plain #Action_bar,.header-stack #Action_bar{background-color:#5c4c52;padding:2px;font-size:15px}
-            div#Action_bar a{color:#fff!important}
-            .why_choose_box img{width:75px}
-            .why_choose_box p{font-size:15px;font-weight:800;color:#000}
-            .why_choose_box div{padding:20px}
-            #nr td,th{text-align:left!important}
-            .small{text-transform:lowercase}
-            .header-stack #Top_bar .logo{border-bottom:1px solid #020202!important}
-            .header-stack.header-center #Top_bar:not(.is-sticky) .menu_wrapper{border-bottom:1px solid #000!important}
-            #Top_bar .menu > li > a span{border-color:#000!important}
-            .leftheading{font-size:19px!important;letter-spacing:1px!important;line-height:25px!important;font-weight:400!important}
-            aside#text-7 h4{font-size:17px!important;letter-spacing:1px!important;font-weight:400!important}
-            aside#text-21 h4{font-size:17px!important;letter-spacing:1px!important;font-weight:400!important}
-            aside#text-19 h4{font-size:17px!important;letter-spacing:1px!important;font-weight:400!important;font-size:17px!important;letter-spacing:1px!important;font-weight:normal!important}
-            aside#text-20 h4{font-size:17px!important;letter-spacing:1px!important;font-weight:400!important}
-            .header-stack #Action_bar .contact_details li,.header-stack #Action_bar .social,.header-stack #Action_bar .social-menu{padding:3px 0!important}
-            #Top_bar .menu > li > a span:not(.description){padding:0 15px!important;border-right-width:2px!important}
-            #Top_bar .menu > li > a{padding:15px 0;font-weight:600}
-            @media only screen and (min-width: 1240px) {
-            #Top_bar.is-sticky .menu_wrapper .menu > li > a{padding:9px 0!important}
-            }
-            #contact-page-div-mahi .wrap.mcb-wrap.one-fourth.co.valign-top.clearfix{height:420px;padding:20px 20px 10px;position:relative}
-            #contact-page-div-mahi .wrap.mcb-wrap.one-fourth.co.valign-top.clearfix input{position:absolute;bottom:10px;left:50%;-webkit-transform:translateX(-50%);transform:translateX(-50%)}
-            #Footer b{font-size:17px;font-weight:700;color:#f6c635!important}   
-        </style>
+        <script type="text/javascript">function setREVStartSize(a){try{var b,c=document.getElementById(a.c).parentNode.offsetWidth;if(c=0===c||isNaN(c)?window.innerWidth:c,a.tabw=void 0===a.tabw?0:parseInt(a.tabw),a.thumbw=void 0===a.thumbw?0:parseInt(a.thumbw),a.tabh=void 0===a.tabh?0:parseInt(a.tabh),a.thumbh=void 0===a.thumbh?0:parseInt(a.thumbh),a.tabhide=void 0===a.tabhide?0:parseInt(a.tabhide),a.thumbhide=void 0===a.thumbhide?0:parseInt(a.thumbhide),a.mh=void 0===a.mh||""==a.mh?0:a.mh,"fullscreen"===a.layout||"fullscreen"===a.l)b=Math.max(a.mh,window.innerHeight);else{for(var d in a.gw=Array.isArray(a.gw)?a.gw:[a.gw],a.rl)(void 0===a.gw[d]||0===a.gw[d])&&(a.gw[d]=a.gw[d-1]);for(var d in a.gh=void 0===a.el||""===a.el||Array.isArray(a.el)&&0==a.el.length?a.gh:a.el,a.gh=Array.isArray(a.gh)?a.gh:[a.gh],a.rl)(void 0===a.gh[d]||0===a.gh[d])&&(a.gh[d]=a.gh[d-1]);var e,f=Array(a.rl.length),g=0;for(var d in a.tabw=a.tabhide>=c?0:a.tabw,a.thumbw=a.thumbhide>=c?0:a.thumbw,a.tabh=a.tabhide>=c?0:a.tabh,a.thumbh=a.thumbhide>=c?0:a.thumbh,a.rl)f[d]=a.rl[d]<window.innerWidth?0:a.rl[d];for(var d in e=f[0],f)e>f[d]&&0<f[d]&&(e=f[d],g=d);var h=c>a.gw[g]+a.tabw+a.thumbw?1:(c-(a.tabw+a.thumbw))/a.gw[g];b=a.gh[g]*h+(a.tabh+a.thumbh)}void 0===window.rs_init_css&&(window.rs_init_css=document.head.appendChild(document.createElement("style"))),document.getElementById(a.c).height=b,window.rs_init_css.innerHTML+="#"+a.c+"_wrapper { height: "+b+"px }"}catch(a){console.log("Failure at Presize of Slider:"+a)}};
+        </script>
+        <link rel="stylesheet" id="wp-custom-css" href="assets/css/wp-custom-css.css" type="text/css" media="all" />
         <noscript>
             <style type="text/css"> .wpb_animate_when_almost_visible { opacity: 1; }</style>
         </noscript>
