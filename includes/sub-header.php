cb36e14a95504d405b210319a16671e6c134a6c6
Jatin Bhatt <jatin.b.php@gmail.com>
2023-07-14 19:06:16 +0530

    changes

--- includes/sub-header.php
+++ includes/sub-header.php
@@ -3,10 +3,10 @@ $pageName = basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']); ?
 <div id="Subheader" style>
     <div class="container">
         <div class="column one">
-            <h1 class="title"><?php echo ucfirst(str_replace("-", " ", str_replace(".php", "", $pageName))); ?></h1>
+            <h1 class="title"><?php echo ucwords(str_replace("-", " ", str_replace(".php", "", $pageName))); ?></h1>
             <ul class="breadcrumbs has-link">
                 <li><a href="index.php">Home</a> <span><i class="icon-right-open"></i></span></li>
-                <li><a href="javascript:void(0);"><?php echo ucfirst(str_replace("-", " ", str_replace(".php", "", $pageName))); ?></a></li>
+                <li><a href="javascript:void(0);"><?php echo ucwords(str_replace("-", " ", str_replace(".php", "", $pageName))); ?></a></li>
             </ul>
         </div>
     </div>
